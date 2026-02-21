d24659f4c222b0397f406045cb063b1e103df0bd
Ryder7223 <114200735+ryder7223@users.noreply.github.com>
2026-02-21 18:51:36 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,7 @@ using namespace geode::prelude;
 
 // Auto Practice Modifier
 class $modify(PlayLayer) {
-
-    struct Fields {
-        ListenerHandle keybindListener;
-    };
-
-    // Hooks a function probably
+    // Hooks the init function
     bool init(GJGameLevel* level, bool p1, bool p2) {
         // If you aren't in a level it won't crash your game
         if (!PlayLayer::init(level, p1, p2))
@@ -19,7 +14,6 @@ class $modify(PlayLayer) {
         bool allowNormalMode = Mod::get()->getSettingValue<bool>("enable-in-normal");
         bool allowTestMode = Mod::get()->getSettingValue<bool>("enable-in-testmode");
         bool allowPlatformerMode = Mod::get()->getSettingValue<bool>("enable-in-platformer");
-        bool shouldEnable = true;
 
         // Disables auto practice if any of these conditions are met
         if (!this->m_isPracticeMode && !m_isTestMode && !m_level->isPlatformer() && allowNormalMode) {
@@ -30,24 +24,17 @@ class $modify(PlayLayer) {
             this->togglePracticeMode(true);
         }
 
-        // Listener for keybind setting
-        m_fields->keybindListener =
-            listenForKeybindSettingPresses("toggle-practice",
-                [this](const Keybind& keybind, bool down, bool repeat, double timestamp) {
-                    if (down && !repeat) {
-                        // Another normal mode check!!!111
-                        this->togglePracticeMode(!this->m_isPracticeMode);
-                    }
+        // Event listener style for keybinds inside PlayLayer
+        this->addEventListener(
+            KeybindSettingPressedEventV3(Mod::get(), "toggle-practice"),
+            [this](const Keybind& keybind, bool down, bool repeat, double timestamp) {
+                if (down && !repeat) {
+                    // Toggle practice mode when key pressed
+                    this->togglePracticeMode(!this->m_isPracticeMode);
                 }
-            );
+            }
+        );
 
         return true;
     }
-
-    void onExit() {
-        // Cancel listener before layer is destroyed
-        m_fields->keybindListener.destroy();
-        PlayLayer::onExit();
-    }
 };
-
